6f0f5319f1a97e84fe0b396a38abfae0f53e23d4
MaykonRO <maykonro2004@gmail.com>
2026-08-13 21:56:41 -0300

    feat: cria a aplicação em java

---
+++ appFatecHub/src/main/java/org/fatec/appfatechub/AppFatecHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.fatec.appfatechub;
+
+/**
+ *
+ * @author mauro
+ */
+public class AppFatecHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
